9efdb2b08edba41282963a5b9758f412d7c26e84
Ruhani Kapoor <ruhanikapoor04@gmail.com>
2024-09-03 12:47:29 +0530

    save tasks to local storage

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -13,19 +13,37 @@ export class TodoComponent {
   tasks: {name: string, completed: boolean}[] = [];
   newTask: string = '';
 
+  constructor(){
+    this.loadTasksFromLocalStorage();
+  }
+
   addTask(){
     if(this.newTask.trim()){
       this.tasks.push({name: this.newTask, completed: false});
       this.newTask = '';
+      this.saveTasksToLocalStorage();
     }
   }
 
   deleteTask(index: number){
     this.tasks.splice(index, 1);
+    this.saveTasksToLocalStorage();
   }
 
   toggleCompletion(index: number){
     this.tasks[index].completed = !this.tasks[index].completed;
+    this.saveTasksToLocalStorage();
+  }
+
+  saveTasksToLocalStorage(){
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+  loadTasksFromLocalStorage(){
+    const storedTasks = localStorage.getItem('tasks');
+    if(storedTasks){
+      this.tasks = JSON.parse(storedTasks);
+    }
   }
 
 }
